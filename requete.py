c6d35d5b46b1081684fe33ec07f1b61bb2f8b0bc
root <root@localhost.localdomain>
2016-04-20 16:11:33 +0200

    requete.py

---
+++ requete.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python2.7
+
+import urllib
+import urllib2
+import time
+import os, sys
+
+#User_agent par defaut
+User_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+
+
+
+#Pour chronometrer le temps de reponse du serveur
+class Timer:
+
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
+
+
+
+
+class HTTP:
+
+    # Peut etre initialise avec les paramatres ou sans
+    def __init__(self, url='', user_agent=User_agent, cookie='', referer='', post=''):
+
+	self.url        = url			# N'oubliez pas le http://
+	self.user_agent = user_agent
+	self.cookies    = cookie		# cookie=valeur;id=42
+	self.referer    = referer
+	self.post       = post			# user=toto;pass=123
+	self.aff_time   = 0
+	self.aff_header = 0
+	self.aff_page   = 1
+
+
+    # Envoie une requete avec les parametres precedement passe
+    def get(self):
+
+	self.response = ''			# Contenu de la page HTML
+	self.header   = ''			# Header reponse serveur
+	self.time     = -1			# Temps de la requete
+	self.error    = -1			#   1 = OK   0 = KO
+
+	# Sort de la fonction si l'url est vide
+	if self.url == '':
+	    return 0
+
+	# Lancement du timer
+        try:
+            with Timer() as t:
+		self.__download()
+	# Fin du timer
+        finally:
+	    # Si aucune erreurs
+	    if self.error == 0:
+		# Recuperation: timer, header et contenu de la page HTML
+	        self.time     = t.interval
+	        self.response = self.__requete.read()
+	        self.header   = self.__requete.info()
+		return 1
+
+	return 0
+
+
+    # Affichage
+    def display(self):
+
+        # Si aucune erreurs
+	if self.error == 0:
+	    if self.aff_page == 1:
+	        print self.response	# Affiche la page sauf si -n
+	    if self.aff_header == 1:
+	        print self.header	# Affiche le header si -i
+	    if self.aff_time == 1:
+	        print self.time		# Affiche le temps si -t
+
+
+    def __download(self):
+
+	# Si aucun referer n'est parametre, la racine du site et prise par defaut
+        if self.referer == '':
+            host = self.url.split('/')
+            referer = "http://" + host[2]
+	else:
+	    referer = self.referer
+
+	# Preparation du header de la requete
+        if self.cookies == '':
+	    header={'User-agent': self.user_agent, 'Referer' : referer}
+        else:
+            header={'User-agent': self.user_agent, 'Cookie': self.cookies, 'Referer' : referer}
+
+	# encodage des variables POST
+    	data = urllib.urlencode(self.__param(self.post))
+	# Preparation de la requete
+    	req  = urllib2.Request(self.url, data, header)
+
+    	try:
+	    # Envoi de la requete
+            self.__requete = urllib2.urlopen(req)
+	    self.error    = 0
+    	except urllib2.HTTPError:
+	    # En cas d'erreur
+	    self.error 	  = 1
+	    self.__requete = None
+
+
+    # Formate les variables post pour les requetes
+    def __param(self, parametre):
+
+	params = {}
+	if parametre == '':
+            return params
+
+	var = parametre.split(';')		# Les variables sont separes les une des autres par des ;
+	count = 0
+	while count < len(var):			# On parcours toutes les variables
+	    split = var[count].split('=')	# La variable et suivit d'un = puis de sa valeur
+	    param = {split[0] : split[1]}	# On place la varible et sa valeur dans un array
+	    params.update(param)		# On l'ajout a la liste
+	    count += 1
+
+	return params
+
+
+    # Pour le main
+    def argv(self, argv):
+
+	self.url = argv[1]
+	if argv[1] == '-h':
+	    self.__help()
+
+    	if len(argv) > 1:
+            num = 1
+            # Parcours les arguments passe au programme
+            while len(argv) > num + 1:
+                num += 1
+	        if argv[num]   == '-h':
+	            self.__help()
+	        if argv[num]   == '-t' or argv[num]   == '--time':
+		    self.aff_time   =     1
+	        if argv[num]   == '-i' or argv[num]   == '--info':
+		    self.aff_header =     1
+	        if argv[num]   == '-n' or argv[num]   == '--no-page':
+		    self.aff_page   =     0
+		if argv[num-1] == '-c' or argv[num-1] == '--cookies':
+		    self.cookies    =     argv[num]
+		if argv[num-1] == '-r' or argv[num-1] == '--referer':
+		    self.referer    =     argv[num]
+		if argv[num-1] == '-u' or argv[num-1] == '--user_agent':
+		    self.user_agent =     argv[num]
+		if argv[num-1] == '-p' or argv[num-1] == '--post-variables':
+		    self.param      =     argv[num]
+
+
+    # Affiche l'aide
+    def __help(self):
+	print 'python',sys.argv[0],'[URL] "User:user_agent" "Var:Variable_post=valeur;id=42" "Ref:referer.com" "Cookie:id=42&n=1" -i -t'
+	sys.exit(0)
+
+
+
+if __name__ == '__main__':
+
+    requete = HTTP()
+
+    # Si les parametres sont passes en argument
+    if len(sys.argv) > 1:
+        requete.argv(sys.argv)
+        if requete.get() == 1:
+       	    requete.display()
+    else:
+	# Exemple
+    	requete.url        = 'http://perdu.com'			# URL
+    	requete.referer    = 'site.com' 			# REFERER
+    	requete.user_agent = 'iphone9' 				# USER_AGENT
+    	requete.post       = 'test=42;test2=jfk;cookie=1239083'	# POST
+    	requete.cookies    = 'cookie1=test;cookie2=pass'	# COOKIES
+
+	# On envoi la requete et on verifie si elle n'a pas rencontre d'erreur
+    	if requete.get() == 1:
+		# On affiche le header de la requete, le contenu de la page et le temps de la requete
+		print requete.header
+		print requete.response
+		print requete.time
+
+    sys.exit(0)
